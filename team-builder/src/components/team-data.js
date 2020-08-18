13b6ea53a99c3e5a2a83ba5aab7dea4756c03099
Matthew <mattk.development@gmail.com>
2020-08-18 16:44:11 -0500

    Mvp

--- team-builder/src/components/team-data.js
+++ team-builder/src/components/team-data.js
@@ -1,20 +1,21 @@
-const teamData = {
+const teamData = [
     {
         name: 'Sidney Crosby',
         role: 'Center',
         email: 'sidthekid@hockey.com'
-    }
+    },
 
     {
         name: 'Evgeni Malkin',
         role: 'Center',
         email: 'evgeni@malkin.com'
-    }
+    },
+    
     {
         name: 'Kris Letang',
         role: 'Defensemen',
         email: 'kris@letang.com'
     }
-}
+]
 
 export default teamData
--- team-builder/src/components/team-member.js
+++ team-builder/src/components/team-member.js
@@ -1,5 +1,19 @@
 import React from 'react'
+import teamData from './team-data'
 
+const Team = props => {
+    return (
+        <div>
+            {props.members.map(props => (
+                <div>
+                    <h2>Name: {props.name}</h2>
+                    <h3>Position: {props.role}</h3>
+                    <h3>Email: {props.email}</h3>
+                </div>
+            ))}
+        </div>
+    )
+}
 
 
 export default Team
