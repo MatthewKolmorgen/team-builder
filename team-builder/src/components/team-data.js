e71ba20f62d95ece2262d83d3a7c01c73d5c6bf1
Matthew <mattk.development@gmail.com>
2020-08-18 14:11:06 -0500

    Created app, Created Components

---
+++ team-builder/src/components/team-data.js
@@ -0,0 +1,20 @@
+const teamData = {
+    {
+        name: 'Sidney Crosby',
+        role: 'Center',
+        email: 'sidthekid@hockey.com'
+    }
+
+    {
+        name: 'Evgeni Malkin',
+        role: 'Center',
+        email: 'evgeni@malkin.com'
+    }
+    {
+        name: 'Kris Letang',
+        role: 'Defensemen',
+        email: 'kris@letang.com'
+    }
+}
+
+export default teamData
